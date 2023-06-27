7fd8acf54d04f0b6d26ca64a0ed5acd5c19a5516
RyaWcksn <pram.aryawcksn@protonmail.ch>
2023-06-27 22:56:57 +0700

    feat(Update)

--- product/src/application/handler/response.ts
+++ product/src/application/handler/response.ts
@@ -2,5 +2,7 @@ import { Product } from "../../domain/products/entity";
 
 export interface GetAllProductResponse {
 	code: Number
+	page: string
+	pageSize: string
 	data: Product[]
 }
--- product/src/application/service/service.ts
+++ product/src/application/service/service.ts
@@ -3,6 +3,7 @@ import { Product } from "../../domain/products/entity";
 import { ProductInterface } from "../../domain/products/productRepository";
 import { CustomError } from "../../utils/error/error";
 import { Logger } from "../../utils/logger/logger";
+import { GetAllQueryParam } from "../handler/request";
 import { ServiceInterface } from "./serviceRepository";
 
 export class ServiceImpl implements ServiceInterface {
@@ -12,21 +13,10 @@ export class ServiceImpl implements ServiceInterface {
 		this.log = log;
 		this.productRepo = product;
 	}
-	async getAllProduct(): Promise<Product[]> {
+	async getAllProduct(payload: GetAllQueryParam): Promise<Product[]> {
 		this.log.log("Service layer")
 		try {
-			// const dummyData: Product[] = [
-			// 	{
-			// 		id: "123",
-			// 		name: "test",
-			// 		sku: "123",
-			// 		image: "123",
-			// 		price: 123,
-			// 		stock: 123,
-
-			// 	},
-			// ]
-			const dummyData = await this.productRepo.getAllProduct();
+			const dummyData = await this.productRepo.getAllProduct(payload);
 			return dummyData
 
 		} catch (e) {

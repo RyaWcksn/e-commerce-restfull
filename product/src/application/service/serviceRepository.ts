1a8c71ac22d906f38b638d5101c7fb14c8167dd4
RyaWcksn <pram.aryawcksn@protonmail.ch>
2023-06-28 22:42:40 +0700

    feat(Product): DEV-0004 | Finish get product detail by SKU
    Finish get product detail by sku

--- product/src/application/service/serviceRepository.ts
+++ product/src/application/service/serviceRepository.ts
@@ -1,7 +1,8 @@
 import { Product } from "../../domain/products/entity";
-import { GetAllQueryParam } from "../handler/request";
+import { GetAllQueryParam, GetParam } from "../handler/request";
 
 export interface ServiceInterface {
 	getAllProduct(payload: GetAllQueryParam): Promise<Product[]>;
 	syncAllProduct(): Promise<void>;
+	getProductDetail(payload: GetParam): Promise<Product>;
 }
